2d0a83f0383f7a33fc9ff46f0aef8bdf8e37c2ed
Robin Worreby <rworreby@student.ethz.ch>
2025-10-21 11:36:16 +0200

    Add argparse to main

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import time
-from bar_simulation import Simulation
-from dataclasses import dataclass
+import timeit
+from bar_simulation.simulation import Simulation
 
 """
 Bar Simulation main file.
@@ -13,25 +12,31 @@ Features:
 -
 """
 
+
 def parse_args(argv=None) -> argparse.Namespace:
     p = argparse.ArgumentParser(description="Simulation boilerplate")
-    p.add_argument("--duration", type=float, default=10.0, help="Simulated duration in weeks")
-    p.add_argument("--output", type=str, default=None, help="Directory to write snapshots")
+    p.add_argument("--duration", type=float, default=100, help="Simulated duration in weeks")
+    p.add_argument("--output_dir", type=str, default="output/", help="Directory to write snapshots")
     p.add_argument("--log", type=str, default="INFO", help="Logging level")
     p.add_argument("--strategy", type=str, default="default", help="Strategy to use in the simulation")
+    p.add_argument("--past-attendenece", type=str, default=[44, 78, 56, 15, 23, 67, 84, 34, 45, 76, 40, 56, 22, 35], help="List with past attendence data")
     args = p.parse_args(argv)
-
-    args.output_dir = args.output
-    args.log_level = args.log
 
     return args
 
 
 def main(argv=None):
     cfg = parse_args(argv)
+
+    start_time = timeit.default_timer()
+    
     sim = Simulation(cfg)
-    sim.initialize()
-    sim.run()
+
+    end_time = timeit.default_timer()
+    print(f"Simulation took {end_time - start_time} seconds")
+
+    print(cfg)
+    print(sim)
 
 
 if __name__ == "__main__":
