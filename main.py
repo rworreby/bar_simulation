86bfb92b7fa9f925311653e9c5eefcbecb710e3b
Robin Worreby <rworreby@student.ethz.ch>
2025-10-21 11:41:07 +0200

    Style fix in main.py

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ Features:
 
 
 def parse_args(argv=None) -> argparse.Namespace:
-    p = argparse.ArgumentParser(description="Simulation boilerplate")
+    p = argparse.ArgumentParser(description="Simulation setup parameters")
     p.add_argument("--duration", type=float, default=100, help="Simulated duration in weeks")
     p.add_argument("--output_dir", type=str, default="output/", help="Directory to write snapshots")
     p.add_argument("--log", type=str, default="INFO", help="Logging level")
-    p.add_argument("--strategy", type=str, default="default", help="Strategy to use in the simulation")
-    p.add_argument("--past-attendenece", type=str, default=[44, 78, 56, 15, 23, 67, 84, 34, 45, 76, 40, 56, 22, 35], help="List with past attendence data")
+    p.add_argument("--strategy", type=str, default="default",
+                   help="Strategy to use in the simulation")
+    p.add_argument("--past-attendenece", type=str,
+                   default=[44, 78, 56, 15, 23, 67, 84, 34, 45, 76, 40, 56, 22, 35],
+                   help="List with past attendence data")
     args = p.parse_args(argv)
 
     return args
@@ -29,7 +32,7 @@ def main(argv=None):
     cfg = parse_args(argv)
 
     start_time = timeit.default_timer()
-    
+
     sim = Simulation(cfg)
 
     end_time = timeit.default_timer()
