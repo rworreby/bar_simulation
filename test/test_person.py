47053af20cb21fc39550a202fd9cd01c9e184716
Robin Worreby <rworreby@student.ethz.ch>
2025-10-21 14:37:00 +0200

    Add tests for person class

--- test/test_person.py
+++ test/test_person.py
@@ -1,15 +1,19 @@
 from bar_simulation.person import Person
+import unittest
 
 
-def main():
-    p1 = Person()
-    print("Number of instances in Person:", Person.get_no_instances())
-    p2 = Person()
-    p3 = Person()
-    print("Number of instances in Person:", Person.get_no_instances())
-    del p3
-    print("Number of instances in Person:", Person.get_no_instances())
+class TestPerson(unittest.TestCase):
+    def test_instance_count_increases(self):
+        Person()
+        n_inst = Person.get_num_instances()
+        Person()
+        self.assertEqual(Person.get_num_instances(), n_inst + 1)
+
+    def test_id_assignment(self):
+        p1 = Person()
+        p2 = Person()
+        self.assertNotEqual(p1.id, p2.id)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
