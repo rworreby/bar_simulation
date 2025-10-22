0d250cc52bb3d8b907a4dff04af48693f3e65d72
Robin Worreby <rworreby@student.ethz.ch>
2025-10-22 17:39:37 +0200

    Add logic for getting strategy in Person

--- bar_simulation/person.py
+++ bar_simulation/person.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
 class Person():
     """Class to represent a person (i.e. bargoer).
 
@@ -12,14 +13,29 @@ class Person():
     # Number of the instances of class Person
     id_number = 0
 
-    def __init__(self):
+    def __init__(self, strategy=None):
         Person.id_number += 1
         self.id = Person.id_number
+        self.strategy_name = strategy
+        self.attendence = []
+
+        if self.strategy_name == "arthur":
+            from bar_simulation.strategy import ArthurStrategy
+            self.strategy = ArthurStrategy()
+        elif self.strategy_name == "naive":
+            from bar_simulation.strategy import NaiveStrategy
+            self.strategy = NaiveStrategy()
+        elif self.strategy_name == "bell":
+            from bar_simulation.strategy import BellSetharesStrategy
+            self.strategy = BellSetharesStrategy()
+        else:
+            from bar_simulation.strategy import NaiveStrategy
+            self.strategy = NaiveStrategy()
 
     def __str__(self) -> str:
         return f"Person ID: {self.id}"
 
     @classmethod
-    def get_num_instances(cls):
-        """Returns number of instances of class."""
+    def get_max_id_instances(cls):
+        """Returns current id number of new instances of class."""
         return cls.id_number
